6625cb121d60152d2995fc61381994d89bf2f5be
Allan Egidio <allan.egidio@outlook.com>
2023-01-03 12:45:45 -0300

    add um commit para ver no changelog

--- lib/elixir_release_web/controllers/page_controller.ex
+++ lib/elixir_release_web/controllers/page_controller.ex
@@ -4,6 +4,7 @@ defmodule ElixirReleaseWeb.PageController do
   def home(conn, _params) do
     # The home page is often custom made,
     # so skip the default app layout.
+    IO.inspect("Uma release d")
     render(conn, :home, layout: false)
   end
 end
